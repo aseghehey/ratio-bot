d627fde44027e32ca8a78751b552aaca1a78b50f
Emanuel <emanuelaseghey9@gmail.com>
2022-06-18 22:07:14 -0400

    added map, counter, arrays, fail safe, follower check and more (maybe)

--- rbot2022.py
+++ rbot2022.py
@@ -64,6 +64,33 @@ while True:
     time.sleep(15) # reloads mentions every 15 seconds
 
 '''
+#array for guy who ratiod, guy who got ratiod and for no ratio found
+WratioArr = ["Ice cold ratio g", "The ratio is outstanding", "Ratio + W + YB better", ""]
+NoRatioArr = ["Stop wasting my time",""]
+LratioArr = ["L + YB better"]
+
+RatioMap = {}
+
+def validate(tweet): # validates og and parent
+    if tweet.in_reply_to_status_id is not None: # if it has a prev
+        temp1 = api.get_status(tweet.in_reply_to_status_id) # create temp to check og
+        if temp1.in_reply_to_status_id is not None: # if og exists
+            return True 
+    return False
+
+ratiocounter = 0 #keeps track of how many ratios the ratio bot has detected
+followertest = api.get_follower_ids()
+print(followertest)
+
+def mapcount(id): # given an "id_str" will return count of that id
+    if id not in RatioMap:
+        RatioMap[id] = 1
+    else:
+        RatioMap[id] += 1
+    return RatioMap[id]
+
+
+
 
 while True:
     mentions = api.mentions_timeline(since_id=mention_id) 
@@ -71,18 +98,34 @@ while True:
         mention_id = mention.id
         try:
             print("trying") 
-            prev_tweet = api.get_status(mention.in_reply_to_status_id)
-            prevprev = api.get_status(prev_tweet.in_reply_to_status_id)
+            if validate(mention):
+                prev_tweet = api.get_status(mention.in_reply_to_status_id)
+                prevprev = api.get_status(prev_tweet.in_reply_to_status_id)
+                
+                # get both counts
+                prevcount = prevprev.favorite_count
+                cnt = prev_tweet.favorite_count
 
-            prevcount = prevprev.favorite_count
-            cnt = prev_tweet.favorite_count
-            print(prevprev.user.screen_name)
-            if cnt > prevcount:
-                api.update_status(f"@ {prev_tweet.user.screen_name} ratiod @ {prevprev.user.screen_name}", in_reply_to_status_id=mention.id_str,auto_populate_reply_metadata=True)
-            else:
-                api.update_status("No ratio detected", in_reply_to_status_id=mention.id_str,auto_populate_reply_metadata=True)
-            print("success")
-            print(prev_tweet.text)
+                print(prevprev.user.screen_name) # test for us
+
+                if cnt > prevcount:
+                    if prev_tweet.id_str in followertest:
+                        Wcount = mapcount(prev_tweet.id_str)
+                        # diff message if followers... which contains W count
+                    else:
+                        #message with no W count
+                        pass
+                    api.update_status(f"@ {prev_tweet.user.screen_name} ratiod @ {prevprev.user.screen_name}", in_reply_to_status_id=mention.id_str,auto_populate_reply_metadata=True)
+                    ratiocounter+=1 
+                else:
+                    api.update_status("No ratio detected", in_reply_to_status_id=mention.id_str,auto_populate_reply_metadata=True)
+
+                print("success")
+                print(prev_tweet.text)
+            
+            # something sassy if format is invalid
+            # api.update_status("No ratio detected", in_reply_to_status_id=mention.id_str,auto_populate_reply_metadata=True)
+
         except Exception as err:
             print(err)
         
