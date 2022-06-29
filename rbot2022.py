0f6522a3c3039d15765f677442ad30380def6bfe
easeghehey <98903704+easeghehey@users.noreply.github.com>
2022-06-28 22:57:22 -0400

    cleaned up code

--- rbot2022.py
+++ rbot2022.py
@@ -21,43 +21,51 @@ api = tweepy.API(_get_auth_(), wait_on_rate_limit=True)
 # except:
 #     print("couldn't verify")
 
-mention_id = 1 # will be used to keep track of the mentions we have gone through
-
 ''' Variables: '''
 # Arrays for guy who ratiod, guy who got ratiod and for no ratio found
 WratioArr = ["ratio + no one cares about that tweet + guy who tweeted it needs to get a job 💀","ratio + YB better + delete cringe tweet + no one really asked 💀","ratio + tweet's not even funny + didn't laugh 😐 + cancelled","after checking VAR 🎥 I have carefully decided to award the ratio.","after reviewing the tweets, me and my team of officials have detected a ratio!","immaculate ratio must be framed 🖼","someone took the L and is down horrendously","tell that guy he got ratio'd + the tweet is irrelevant + no one cares","ratio bot decides to award the ratio here.","what an outstanding ratio. buddy above got cooked 😩","ratio so fire it needs to be framed","ice cold ratio", "outstanding ratio", "ratiooooo", "ratio detected!","W","fire ratio", "VAR DECISION: ratio", "ratio identified + W", "we have uncovered a remarkable ratio","successful ratio!"]
 NoRatioArr = ["hmmmmm 🤔that doesn't look like a ratio to me","failed ratio + YB better","ratio is no good + not found","ratio nowhere to be found","stop wasting my time there's no ratio","stop being silly there's no ratio", "no ratio as of rn", "no ratio found","come on there's no ratio there...", "no ratio g", "ratio denied", "failed ratio lol"]
 
+# had to stop using LratioArr because twitter complained that it violated the guidelines
 LratioArr = ["L + ratio + YB better", "hold this L", "ratio + L + get a job", "ratiooood","hold this L respectfully", "ratio + L", "down bad", "down horrendoulsy", "invalid argument + ratio","hold this L son", "ratio bozo","hold this L buddy", "buddy got cooked","failed ratio"]
+
+# array with images
 ratio_img_arr = ["pics/ratio/checkingratio.png","pics/ratio/decisionratio.jpeg","pics/ratio/rratio.jpg","pics/ratio/ratio10.png","pics/ratio/IMG_0303.JPG"]
 no_ratio_img_arr = ["pics/noratio/no ratio.jpeg","pics/noratio/noratio1.jpeg","pics/noratio/ratiodenied.jpeg","pics/noratio/vardecision_noratio.jpeg","pics/noratio/IMG_0302.JPG"]
 
+# maps to keep track of wins, losses and draws
 wmap = {}
 lmap = {}
 dmap = {}
+
+# was going to use to count how many ratios, but not needed since i can see how many tweets the bot has put out
 ratiocounter = 1 
 
+# check if tweet is protected because bot cant reply to those
 def _isprotected_(tweet):
     if tweet.user.protected:
         return True
     return False
 
 ''' Functions: '''
-def validateRatioFormat(tweet): # validates og and parent
-    if tweet.in_reply_to_status_id is not None: # if it has a prev
-        temp1 = status(tweet.in_reply_to_status_id) # create temp to check og
+
+def validateRatioFormat(tweet): # validates that there are two tweets above (correct format)
+    if tweet.in_reply_to_status_id is not None: 
+        temp1 = status(tweet.in_reply_to_status_id) 
         if not _isprotected_(temp1):
-            if temp1.in_reply_to_status_id is not None: # if og exists
+            if temp1.in_reply_to_status_id is not None: 
                 temp2 = status(tweet.in_reply_to_status_id)
                 if not _isprotected_(temp2):
                     return True 
     return False
 
+# checks if the map is empty, would be used for the weekly wrapped, if i did it
 def isMapEmpty(map):
     if not map:
         return True
     return False
 
+# adding to map
 def mapcount(id, RatioMap): # given an "id_str" will return count of that id
     if id not in RatioMap:
         RatioMap[id] = 1
@@ -65,8 +73,8 @@ def mapcount(id, RatioMap): # given an "id_str" will return count of that id
         RatioMap[id] += 1
     return RatioMap[id]
 
-def acc_status(id): 
 # takes the id of an account and returns their score in an array format: [Ws,Ls, Detections]
+def acc_status(id): 
     resW,resL,resD = 0,0,0
     if id in wmap:
         resW = wmap[id]
@@ -76,12 +84,15 @@ def acc_status(id):
         resD = dmap[id]
     return [resW,resL,resD]
 
+# gives random index from array: done by pratuln (his contribution)
 def RSFromArray(Arr):
     Ridx = random.randint(0,len(Arr)-1)
     return Arr[Ridx]
 
+# Was planning on doing a weekly wrapped, where every Friday at 6pm, it would give the top3 users with Wins, Losses and Detections, however, twitter is not okay
+# with mentioning people without them mentioning the bot first
+# given a map (Win,Loss or Detect) the function returns a list of the top 3 things in the map
 def weeklywrapped(givenmap):
-    # given a map (Win,Loss or Detect) the function returns a list of the top 3 things in the map
     topRatios = []
     hq.heapify(topRatios)
     for key,val in givenmap.items():
@@ -93,6 +104,7 @@ def weeklywrapped(givenmap):
         top3.append(hq.heappop(topRatios))
     return top3
 
+# creates the weekly message
 def messageWeekly():
     #checks that map isnt empty and its length is greater than 3, otherwise we get an error
     if isMapEmpty(wmap) or len(wmap) < 3:
@@ -107,6 +119,8 @@ def messageWeekly():
     content += f"[🥇] {sayings[0]}\n[🥈] {sayings[1]}\n[🥉] {sayings[-1]}\n\nit resets every day at 6pm"
     return content
 
+# would be used to determine when its time to post, but changed the function
+# will now use it for when its 6pm, so bot clears map - for memory purposes
 def timetopostWeekly():
     # post on fridays at 6pm
     dow = datetime.datetime.today().weekday()
@@ -126,26 +140,31 @@ def timetopostWeekly():
 def checkiffollowing():
     pass
 
+#to clear all maps at once
 def clearmaps():
     wmap.clear()
     lmap.clear()
     dmap.clear()
 
+# would post weekly post but not doing it anymore
 def makeWeeklypost():
     if messageWeekly()!="":
         api.update_status(messageWeekly())
         clearmaps()
 
+# posting a reply with picture
 def reply_with_media(tweet_id, message, imagepath):
     # media = api.media_upload(imagepath)
     api.update_status_with_media(message,imagepath,in_reply_to_status_id=tweet_id,auto_populate_reply_metadata=True)
     # api.update_status(message,media,in_reply_to_status_id=tweet_id,auto_populate_reply_metadata=True) #,auto_populate_reply_metadata=True
     print("tweeted with media")
 
+# posting a reply without picture
 def reply_no_media(tweet_id,message):
     api.update_status(message, in_reply_to_status_id=tweet_id,auto_populate_reply_metadata=True)
     print("tweeted no media")
 
+# makes the format of the message, depending on what case it is
 def messageformat(id,option):
     content = ""
     if option == 1: # check ratio
@@ -160,17 +179,20 @@ def messageformat(id,option):
         content = f"{RSFromArray(NoRatioArr)} 😐"
     return content
 
+# adding to maps at once
 def addToMaps(W,L,R):
     mapcount(W.user.id,wmap)
     mapcount(L.user.id,lmap)
     mapcount(R.user.id,dmap)
 
+# calculates if theres a ratio
 def calculateratio(tweetID,prevtweetID):
     if tweetID.favorite_count > prevtweetID.favorite_count:
         return True
     else:
         return False
 
+# given an ID, returns the status/tweet
 def status(tweetID):
     return api.get_status(tweetID)
 
@@ -181,10 +203,12 @@ def validQuoteRatioFormat(mention):
             return True
     return False
 
+# like the random saying array, redundant i know but i forgot i had the other function 
 def _randomratiopic_(arr):
     rand = random.choice(arr)
     return rand
 
+# functionality of the code
 def applyRatio(mentionedtwt, ratiotwt, ratioedtwt):
     if calculateratio(ratiotwt,ratioedtwt):
         addToMaps(ratiotwt,ratioedtwt,mentionedtwt)
@@ -196,6 +220,8 @@ def applyRatio(mentionedtwt, ratiotwt, ratioedtwt):
         reply_with_media(mentionedtwt.id,message,_randomratiopic_(no_ratio_img_arr))
         print(f"no ratio\n{ratiotwt.text}\n{ratioedtwt.text}\n")
 
+# writes current mention ID on a text file so that if the program crashes, it doesnt start from the old mentions
+# goes back to the most recent mention
 file_name = "last_tweet.txt"
 def set_last_seen(last_seen, file_name):
     f_write = open(file_name, 'w')
@@ -211,6 +237,7 @@ def retrieve_last_seen_id(file_name):
     print(f"last seen found {last_seen_id}")
     return last_seen_id
 
+# needed
 def _me_(tweet):
     botid = 1537546826026319872
     if tweet.user.id != botid:
@@ -220,6 +247,7 @@ def _me_(tweet):
 def verify_tweet(tweet_id):
     pass
  
+# execution of functionality
 def replyratio(lastseen):
     # last_seen_id = retrieve_last_seen_id(file_name)
     timeline = api.mentions_timeline(since_id=lastseen) #since_id=last_seen_id
@@ -248,6 +276,8 @@ def replyratio(lastseen):
             message = messageformat(mention,2)
             print(f"ratio account status targeted - {mention.user.screen_name}")
             reply_no_media(mention.id_str,message)
+
+    # Had to give up on this. Twitter didnt like it. Tbf, I get it. It spammed. It kept spamming everytime it was mentioned and the correct format wasnt there. 
         # else: # incorrect format
         #     if mention.in_reply_to_status_id is not None:
         #         check1 = status(mention.in_reply_to_status_id)
@@ -260,30 +290,12 @@ def replyratio(lastseen):
         #         print(f"incorrect format 2 - {mention.text} - {mention.user.screen_name}")
         #         # reply_no_media(mention.id_str,message)
 
+# for testing purposes
 def deleteMentions4testpurposes():
     t = api.user_timeline()
     for i,t1 in enumerate(t):
         if (t1.in_reply_to_status_id is not None) and (t1.in_reply_to_user_id != 1537546826026319872):
             api.destroy_status(t1.id)
-
-# def main():
-#     try:
-#         print("trying")
-#         keep_alive()
-#         # reply_no_media("1539573736126431232","hi @elonhireme_")
-
-#         # print(datetime.datetime.today().time())
-#         # print()
-#         # deleteMentions4testpurposes()
-#         while True:
-#              if timetopostWeekly():
-#                  makeWeeklypost()
-            
-#              replyratio(retrieve_last_seen_id(file_name))
-#              time.sleep(15)
-#         print("done")
-#     except Exception as err:
-#         print(err)
 
 def run():
     while True:
@@ -293,15 +305,7 @@ def run():
         replyratio(retrieve_last_seen_id(file_name))
         time.sleep(15)
 
-# if "__name__" == "__main__":
 try:
-    # u = status(1540572744697495553)
-    # print(validateRatioFormat(u))
-    # print(_isprotected_(status(1540416454956158976)))
     run()
-    # t = status(1540713260072067077)
-    # print(t)
-    # replyratio(retrieve_last_seen_id(file_name))
-    # print(u.user)
 except Exception as err:
     print(err)
